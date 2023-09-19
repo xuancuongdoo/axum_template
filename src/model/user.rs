87e984da541b9840fbd481e680767a623a87e619
xuancuongdoo <cuongxuando209@gmail.com>
2023-09-19 13:03:36 +0700

    ci

--- src/model/user.rs
+++ src/model/user.rs
@@ -7,6 +7,8 @@ use sqlx::postgres::PgRow;
 use sqlx::FromRow;
 use uuid::Uuid;
 
+use super::base::{self, DbBmc};
+
 #[derive(Clone, Fields, FromRow, Debug, Serialize)]
 pub struct User {
     pub id: i64,
@@ -46,3 +48,63 @@ pub trait UserBy: HasFields + for<'r> FromRow<'r, PgRow> + Unpin + Send {}
 impl UserBy for User {}
 impl UserBy for UserForLogin {}
 impl UserBy for UserForAuth {}
+
+pub struct UserBmc;
+
+impl DbBmc for UserBmc {
+    const TABLE: &'static str = "users";
+}
+
+impl UserBmc {
+    pub async fn get<E>(ctx: &Ctx, mm: &ModelManager, id: i64) -> Result<E>
+    where
+        E: UserBy,
+    {
+        base::get::<UserBmc, E>(ctx, mm, id).await
+    }
+
+    pub async fn first_by_username<E>(
+        _ctx: &Ctx,
+        mm: &ModelManager,
+        username: &str,
+    ) -> Result<Option<E>>
+    where
+        E: UserBy,
+    {
+        let db = mm.db();
+        let user = sqlb::select()
+            .table(Self::TABLE)
+            .and_where("username", "=", username)
+            .fetch_optional::<_, E>(db)
+            .await?;
+        Ok(user)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::_dev_utils;
+    use anyhow::{Context, Result};
+    use serial_test::serial;
+
+    #[serial]
+    #[tokio::test]
+    async fn test_first_ok_demo1() -> Result<()> {
+        // -- Setup & Fixtures
+        let mm = _dev_utils::init_test().await;
+        let ctx = Ctx::root_ctx();
+        let fx_username = "demo1";
+
+        // -- Exec
+        let user: User = UserBmc::first_by_username(&ctx, &mm, fx_username)
+            .await?
+            .context("Should have user 'demo1'")?;
+
+        // -- Check
+        assert_eq!(user.username, fx_username);
+
+        Ok(())
+    }
+}
+// endregion: --- Tests
